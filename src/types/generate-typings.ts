04f00c0d37268c5f705bf6613034d654a84e2d85
JangHun <pain3992@gmail.com>
2021-12-14 14:02:46 +0900

    기본설정 from khnp-api 2

--- src/types/generate-typings.ts
+++ src/types/generate-typings.ts
@@ -3,11 +3,13 @@ import { join } from 'path';
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
-  typePaths: ['./src/**/*.graphql'],
+  //   typePaths: ['./src/**/*.graphql'],
+  //   path: join(process.cwd(), 'src/types/graphql.schema.ts'),
+  typePaths: ['./src/**/*.(gql|graphql)'],
   path: join(process.cwd(), 'src/types/graphql.schema.ts'),
   outputAs: 'class',
-  watch: true,
-  defaultScalarType: 'Date',
-  enumsAsTypes: false,
-  debug: true,
+  //   watch: true,
+  //   defaultScalarType: 'Date',
+  //   enumsAsTypes: false,
+  //   debug: true,
 });
